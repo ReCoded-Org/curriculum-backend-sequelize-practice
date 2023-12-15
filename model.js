87256137234b7b62cbe881632da9f91de721abf3
Kutay Kağan Özen <kutay.kagan.ozenn@gmail.com>
2023-12-15 21:02:42 +0300

    Feat: Dockerized

--- model.js
+++ model.js
@@ -1,1 +1,22 @@
 // Enter your code below
+
+const Sequelize = require("sequelize");
+
+const path = "mysql://root:secret@db:3306/practice";
+const sequelize = new Sequelize(path);
+
+// The table will have a column called description of type string
+const Todo = sequelize.define("todo", {
+  description: Sequelize.STRING,
+});
+
+Todo.sync()
+  .then(() => {
+    console.log('New table "todo" created');
+  })
+  .catch((err) => {
+    console.error("Unable to create table: ", err);
+  })
+  .finally(() => {
+    sequelize.close();
+  });
