1643b60eed05d26703b2833b25b75695f05c1c43
Kutay Kağan Özen <kutay.kagan.ozenn@gmail.com>
2024-01-08 19:21:21 +0300

    Fix: EOL changed to LF

--- connect.js
+++ connect.js
@@ -1,1 +1,17 @@
 // Enter your code below
+const Sequelize = require("sequelize");
+
+const path = "mysql://root:secret@db:3306/practice";
+const sequelize = new Sequelize(path);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database: ", err);
+  })
+  .finally(() => {
+    sequelize.close();
+  });
--- multi_insert_todo.js
+++ multi_insert_todo.js
@@ -2,12 +2,12 @@
 // Insert the values from given array below
 
 const todosToInsert = [
-    { description: 'Tai chi in the morning' },
-    { description: 'Visit friend' },
-    { description: 'Visit grocery store' },
-    { description: 'Listen to music' },
-    { description: 'Watch Netlfix' },
-    { description: 'Walk for an hour' }
+  { description: "Tai chi in the morning" },
+  { description: "Visit friend" },
+  { description: "Visit grocery store" },
+  { description: "Listen to music" },
+  { description: "Watch Netlfix" },
+  { description: "Walk for an hour" },
 ];
 
 // Enter your code below
